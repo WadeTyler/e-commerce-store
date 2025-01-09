6f1123bd66189a92cbbf44b67f4348a19e64b055
Tyler Wade <vaxonreal@gmail.com>
2025-01-09 13:17:27 -0500

    added analytics tab

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -48,51 +48,61 @@ const getAnalyticsData = async () => {
   
 }
 
-const getDailySalesData = async (startDate, endDate) => {
-  try {
-    const dailySalesData = await Order.aggregate([
-      {
-        $match: {
-          createdAt: {
-            $gte: startDate,
-            $lte: endDate
-          },
-        },
-      },
-      {
-        $group: {
-          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
-          sales: { $sum: 1 },
-          revenue: { $sum: "$totalAmount" },
-        },
-      },
-      { $sort: { _id: 1 } },
-    ]);
-  
-    const dateArray = getDatesInRange(startDate, endDate);
-  
-    return dateArray.map(date => {
-      const foundData = dailySalesData.find(item => item._id === date);
-      
-      return {
-        date,
-        sales: foundData ? foundData?.sales : 0,
-        revenue: foundData ? foundData?.revenue : 0
-      }
-    });
-  } catch (error) {
-    throw error;
-  }
-}
+export const getDailySalesData = async (startDate, endDate) => {
+	try {
+		const dailySalesData = await Order.aggregate([
+			{
+				$match: {
+					createdAt: {
+						$gte: startDate,
+						$lte: endDate,
+					},
+				},
+			},
+			{
+				$group: {
+					_id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+					sales: { $sum: 1 },
+					revenue: { $sum: "$totalAmount" },
+				},
+			},
+			{ $sort: { _id: 1 } },
+		]);
+
+		// example of dailySalesData
+		// [
+		// 	{
+		// 		_id: "2024-08-18",
+		// 		sales: 12,
+		// 		revenue: 1450.75
+		// 	},
+		// ]
+
+		const dateArray = getDatesInRange(startDate, endDate);
+		// console.log(dateArray) // ['2024-08-18', '2024-08-19', ... ]
+
+		return dateArray.map((date) => {
+			const foundData = dailySalesData.find((item) => item._id === date);
+
+			return {
+				date,
+				sales: foundData?.sales || 0,
+				revenue: foundData?.revenue || 0,
+			};
+		});
+	} catch (error) {
+		throw error;
+	}
+};
 
 function getDatesInRange(startDate, endDate) {
-  const dates = [];
-  let currentDate = new Date(startDate);
+	const dates = [];
+	let currentDate = new Date(startDate);
 
-  while (currentDate <= endDate) {
-    dates.push(new Date(currentDate));
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
+	while (currentDate <= endDate) {
+		dates.push(currentDate.toISOString().split("T")[0]);
+		currentDate.setDate(currentDate.getDate() + 1);
+	}
 
-  return dates;
+	return dates;
 }
